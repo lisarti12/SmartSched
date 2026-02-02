8ee72c6a0cdb23a160a788e98db358dd37fb2151
Lisart Mella <146731565+lisarti12@users.noreply.github.com>
2026-02-02 22:23:11 +0200

    Add files via upload

---
+++ SmartSched/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using SmartSched.DTOs;
+using SmartSched.Data;
+using SmartSched.Models;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace SmartSched.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
